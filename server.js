892b8f1d0271cad10b49966110ad1443b7b2ec4c
tonyqui <tonyqui@hotmail.com>
2018-06-15 15:05:29 -0400

    few more changes

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const fs = require('fs');
 var app = express();
 
 var listenOn = 8080;
-var healthy = false;
+var healthCheck = false;
+var livenessProbe = false;
 
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname+'/public'));
+app.use('/public', express.static(__dirname+'/public'));
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
@@ -18,14 +19,21 @@ app.get('/', (req, res) => {
 });
 
 app.get('/isHealthy', (req,res) => {
-    res.render('isHealthy.hbs', {
+    res.render('healthCheck.hbs', {
         pageTitle: 'Check health of application',
-        healthCheck: healthy
+        healthCheck: healthCheck
     })
-})
+});
+
+app.get('/isAlive', (req,res) => {
+    res.render('livenessProve.hbs', {
+        pageTitle: 'Check readiness of application',
+        livenessProbe: livenessProbe 
+    })
+});
 
 app.get('/healthCheck', (req, res) => {
-    if (healthy) {
+    if (healthCheck) {
         res.writeHead(200);
         res.send();
     }
@@ -33,15 +41,49 @@ app.get('/healthCheck', (req, res) => {
         res.writeHead(404);
         res.send();
     }
-})
+});
 
-app.get('/setAsUnhealthy', (req, res) => {
-    healthy = false;
+app.get('/setAsNotHealthy', (req, res) => {
+    healthCheck = false;
     res.writeHead(200);
     res.send();
-})
+});
+
+app.get('/setAsHealty', (req, res) => {
+    healthCheck = true;
+    res.writeHead(200);
+    res.send();
+});
+
+app.get('/setAlive', (req, res) => {
+    livenessProbe = true;
+    res.writeHead(200);
+    res.send()
+});
+
+app.get('/setDead', (req, res) => {
+    livenessProbe = false;
+    res.writeHead(200);
+    res.send()
+});
+
+app.get('/vote', (req, res) => {
+    console.log(req.query);
+});
+
+app.get('/display', (req, res) => {
+    var data = {
+            datasets: [{
+            data: [10, 20, 30]
+            }],
+        };
+    res.render('displayVotes.hbs', {
+       data: data
+    });
+});
 
 app.listen(listenOn, () => {
     console.log(`Server is now listening on ${listenOn}`);
     healthy = true;
-})
+    livenessProbe = true;
+});
