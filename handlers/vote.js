1b7b938bba3fe72ecf8875cb9266f82babc0cd73
tonyqui <tonyqui@hotmail.com>
2018-06-17 01:22:01 -0400

    Votes are now written on the file system

--- handlers/vote.js
+++ handlers/vote.js
@@ -1,22 +1,50 @@
-var votes = {};
+const fs = require('fs');
+const fileName = 'files/votes.json'
+
+var fetchVotesFromFile = () => {
+    try {
+        var votes = fs.readFileSync(fileName);
+        console.log(`Those are the votes ${JSON.stringify(votes)}`);
+        if (votes.length > 0 ) {
+            return JSON.parse(votes);
+        } else {
+            return {};
+        }
+    } catch(e) {
+        console.log(`Error occurred: ${e}`);
+        return {};
+    }
+}
+
+var saveVotesToFile = (votes) => {
+    console.log(`Writing this to file \n ${JSON.stringify(votes)}`);
+    fs.writeFileSync(fileName, JSON.stringify(votes));
+}
 
 var vote = (aContest, aVote) => {
+    var votes = fetchVotesFromFile();
+    console.log(`Contest -> ${aContest} \n aVote -> ${aVote}\n`);
+    console.log(`Fetched vote -> ${votes}`);
     if (!votes[aContest]) {
+        console.log('inside first if');
         votes[aContest] = {};
-    }
-    if (!votes[aContest][aVote]) {
-        votes[aContest][aVote] = 1;
+        if (!votes[aContest][aVote]) {
+            console.log('inside second if');
+            votes[aContest][aVote] = 1;
+        }
     }
     else {
         votes[aContest][aVote] = votes[aContest][aVote]+1;
     }
+    saveVotesToFile(votes);
 }
 
 var getVotes = () => {
-    return votes;
+    return fetchVotesFromFile();
 }
 
 var getContestVotesAsArrays = (contest) => {
+    var votes = fetchVotesFromFile();
     var aContest = votes[contest];
     var results = [];
     if ( typeof aContest !== 'undefined' && Object.keys(aContest).length > 0) {
